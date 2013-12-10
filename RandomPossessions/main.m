d71ae2f9b18b4c74b30ca88de4a20404b747b346
Todd Patrick <todd.patrick@espn.com>
2013-12-10 13:55:23 -0500

    Initial Commit

---
+++ RandomPossessions/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  RandomPossessions
+//
+//  Created by Todd Patrick on 12/10/13.
+//  Copyright (c) 2013 Todd Patrick. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
